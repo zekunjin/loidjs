d0bd66e4284925f9f2478afa8361b1d72777b976
zekun.jin <zekun.jin@connext.com.cn>
2022-09-19 18:00:10 +0800

    feat: add vite unplugin package

--- packages/plugin/index.ts
+++ packages/plugin/index.ts
@@ -1,19 +1,23 @@
 import { createUnplugin } from 'unplugin'
 // import fg from 'fast-glob'
-import type { RouterOptions } from 'vue-router'
+
+export interface FileBasedRouterOptions {
+  mode: 'hash' | 'history'
+}
 
 export const createAppRE = /\bcreateApp(?:(\w+))/g
 
-export const unpluginFileBasedRouter = createUnplugin((options?: RouterOptions) => {
+export const unpluginFileBasedRouter = createUnplugin((options?: FileBasedRouterOptions) => {
+  console.log('==========  unplugin file based router ==========')
+  console.log(options)
+
   return {
     name: 'unplugin-file-based-router',
-    transformInclude: (id) => id.indexOf('main') > -1,
+    transformInclude: () => true,
     async transform(code) {
-      console.log('==========  unplugin file based router ==========')
-
-      // const entries = await fg(id, { dot: true })
-
       return code
     }
   }
 })
+
+export const vitePluginVueFileBasedRouter = unpluginFileBasedRouter.vite
--- playgrounds/vite/vite.config.ts
+++ playgrounds/vite/vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'node:path'
-import { unpluginFileBasedRouter } from '../../packages/plugin'
+import { vitePluginVueFileBasedRouter } from '../../packages/plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), unpluginFileBasedRouter.vite()],
+  plugins: [vue(), vitePluginVueFileBasedRouter({ mode: 'history' })],
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
