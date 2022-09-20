838736bbb78cf346543ad08524696b56447e46d4
zekun.jin <zekun.jin@connext.com.cn>
2022-09-20 10:33:17 +0800

    chore: rm unused dependencies

--- packages/plugin/index.ts
+++ packages/plugin/index.ts
@@ -1,11 +1,8 @@
 import { createUnplugin } from 'unplugin'
-// import fg from 'fast-glob'
 
 export interface FileBasedRouterOptions {
   mode: 'hash' | 'history'
 }
-
-export const createAppRE = /\bcreateApp(?:(\w+))/g
 
 export const unpluginFileBasedRouter = createUnplugin((options?: FileBasedRouterOptions) => {
   return {
