487db659373d582ed177d8ceee1d65b693a88273
zekun.jin <zekun.jin@connext.com.cn>
2022-10-17 10:47:32 +0800

    feat: add fetch package

--- playgrounds/vite/vite.config.ts
+++ playgrounds/vite/vite.config.ts
@@ -1,7 +1,7 @@
+import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { resolve } from 'node:path'
-import { unpluginFileBasedRouter } from '../../packages/common'
+import { unpluginFileBasedRouter } from '../../packages/common/dist'
 
 // https://vitejs.dev/config/
 export default defineConfig({
